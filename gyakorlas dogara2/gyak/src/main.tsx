9fa36524a9bb4778e18e5ecb92b70a9d2923e06d
Karasz Máté <b1karmat@vasvari.org>
2025-12-05 10:40:55 +0100

    2025. 12. 05. - fad

--- gyakorlas dogara2/gyak/src/main.tsx
+++ gyakorlas dogara2/gyak/src/main.tsx
@@ -1,20 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AllPizza from './pages/AllPizza.tsx'
-import Cart from './pages/Cart.tsx'
-import { Toast, ToastContainer } from 'react-bootstrap'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AllPizza from "./pages/AllPizza";
+import { ToastContainer } from "react-toastify";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Cart from "./pages/Cart";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<AllPizza/>} />
-        <Route path='/cart' element={<Cart />} />
+        <Route path="/" element={<AllPizza />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </BrowserRouter>
-      <ToastContainer/> 
+    <ToastContainer />
   </StrictMode>
-
-)
+);
--- gyakorlas dogara2/gyak/src/pages/Cart.tsx
+++ gyakorlas dogara2/gyak/src/pages/Cart.tsx
@@ -1,8 +1,58 @@
-function Cart() {
-  return (
-    <div>
-      <h1>Cart</h1>
-    </div>
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import apiClient from "../api/apiClient";
+import type { Pizza } from "../types/Pizza";
+import { Button } from "react-bootstrap";
+
+const Cart = () => {
+  // pizzák betöltése
+  const [pizzak, setPizzak] = useState<Pizza[]>([]);
+  useEffect(() => {
+    apiClient
+      .get("/pizzak")
+      .then((response) => setPizzak(response.data))
+      .catch(() => toast.error("Hiba a betöltés során!"));
+  }, []);
+
+  // kosár kezelése
+  const [kosar, setKosar] = useState<number[]>(
+    JSON.parse(localStorage.getItem("kosar") ?? "[]")
   );
-}
+  useEffect(() => {
+    localStorage.setItem("kosar", JSON.stringify(kosar));
+  }, [kosar]);
+
+  // kosár elemének törlése
+  const removeItem = (index: number) => {
+    setKosar(kosar.filter((_, i) => index !== i));
+    toast.success("Sikeres törlés!");
+  };
+
+  // renderelés
+  return (
+    <table>
+      <thead>
+        <th>Név</th>
+        <th>Ár</th>
+        <th>Törlés</th>
+      </thead>
+      <tbody>
+        {kosar.map((value, index) => {
+          const pizza = pizzak.find((p) => Number(p.id) === value);
+
+          return (
+            <tr>
+              <td>{pizza?.nev}</td>
+              <td>{pizza?.ar} Ft</td>
+              <td>
+                <Button onClick={() => removeItem(index)}>Törlés</Button>
+              </td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
 export default Cart;
